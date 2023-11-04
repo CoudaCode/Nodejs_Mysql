a914bc2487cb145ee787a8d0f600e1a4f616a3bd
Diarassouba Mamadou <couda.dm@gmail.com>
2023-11-04 21:54:54 +0000

    ok

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import sequelize from "./config/sequelize.js";
 import path from "path";
 import userRoute from "./routes/users.js";
 import tacheRoute from "./routes/tache.js";
+import {serve,setup} from "swagger-ui-express";
+
+import swagerFIle from "./utils/swagger.json" assert { type: "json" };
 config({
   path: path.join(process.cwd(), ".env.local"),
 });
@@ -14,18 +17,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/api-docs", serve, setup(swagerFIle))
+
 connectDB.connect((err) => {
   if (err) {
     console.log(err);
   } else {
-    sequelize.authenticate().then(() => {
-      console.log("bien connecté a la bd avec sequelize.");
-    }).catch((error) => {
-      console.error("Unable to connect to the database:", error);
-    });
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("bien connecté a la bd avec sequelize.");
+      })
+      .catch((error) => {
+        console.error("Unable to connect to the database:", error);
+      });
     app.listen(PORT, () => {
       console.log("bien connecté !!!");
-      });
+    });
   }
 });
 app.use("/api/users", userRoute);
